bce2417b9053d6b636e8bb3f2bdbf70afcd04ef9
Adam Brenecki <adam@brenecki.id.au>
2013-06-02 15:59:15 +0930

    Double-quote things

--- manifests/package.pp
+++ manifests/package.pp
@@ -6,7 +6,7 @@ define virtualenv::package (
   $path = getparam($env_, 'path')
 
   exec {"ve-${env}-install-${package}":
-    command => '${path}/bin/pip install ${package}',
+    command => "${path}/bin/pip install ${package}",
     require => $env_,
     user => getparam($env_, 'user'),
     group => getparam($env_, 'group'),
--- manifests/requirements.pp
+++ manifests/requirements.pp
@@ -6,7 +6,7 @@ define virtualenv::requirements (
   $path = getparam($env_, 'path')
 
   exec {'ve-${env}-req-${title}':
-    command => '${path}/bin/pip install -r ${file}',
+    command => "${path}/bin/pip install -r ${file}",
     require => $env_,
     user => getparam($env_, 'user'),
     group => getparam($env_, 'group'),
